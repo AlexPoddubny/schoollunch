4354d1b71553091523f81452494e7c4f8fbea9be
AlexPoddubny <usatij.alex@gmail.com>
2020-02-23 00:11:31 +0200

    +BaseModel

---
+++ app/BaseModel.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+abstract class BaseModel extends Model
+{
+    protected static function tableName()
+    {
+        $name = strtolower(static::className());
+        $name = substr($name, strripos($name, '\\') + 1);
+        $l = substr($name, -1);
+        switch ($l){
+            case 's':
+                $name .= 'es';
+                break;
+            case 'y':
+                $name = substr($name, 0, strlen($name) - 1) . 'ies';
+                break;
+            default:
+                $name .= 's';
+                break;
+        }
+        static::$table = $name;
+    }
+    
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        static::tableName();
+    }
+}
