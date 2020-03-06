b8a1e7b31b71191e08c9329ffcd673165f88ead8
AlexPoddubny <usatij.alex@gmail.com>
2020-03-07 01:35:52 +0200

    Schools Model & Migration

---
+++ app/School.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class School extends Model
+{
+    public $timestamps = FALSE;
+    
+    public function admin()
+    {
+        return $this->hasOne('App\Users', 'admin_id');
+    }
+}
