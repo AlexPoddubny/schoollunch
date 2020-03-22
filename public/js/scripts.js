822a19c96aafb4a862953f1667a6833e7cd59a28
AlexPoddubny <usatij.alex@gmail.com>
2020-03-23 00:15:25 +0200

    Children selection (begin)

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,22 +4,3 @@ $(document).on('click', '.add-user', function (e) {
     $('#user-id').attr('value', $(this).data('id'));
     $('.search').html('');
 });
-
-/*$(document).on('click', '.btn-add', function (e) {
-    e.preventDefault();
-
-    var controlForm = $('.controls form:first'),
-        currentEntry = $(this).parents('.entry:first'),
-        newEntry = $(currentEntry.clone()).appendTo(controlForm);
-
-    newEntry.find('input').val('');
-    controlForm.find('.entry:not(:last) .btn-add')
-        .removeClass('btn-add').addClass('btn-remove')
-        .removeClass('btn-success').addClass('btn-danger')
-        .html('<span class="glyphicon glyphicon-minus"></span>');
-    }).on('click', '.btn-remove', function (e) {
-    $(this).parents('.entry:first').remove();
-
-    e.preventDefault();
-    return false;
-});*/
--- resources/views/children.blade.php
+++ resources/views/children.blade.php
@@ -1,9 +1,9 @@
-<form action="{{route('school.select')}}" method="post">
-    @csrf
+<form method="POST" action="{{ route('schoolclass.store') }}">
     <div class="form-group row">
-        <label for="school" class="col-md-4 col-form-label text-md-right">{{__('messages.select_school')}}</label>
+        <label for="lunchbreak" class="col-md-4 col-form-label text-md-right">{{ __('messages.select_school') }}</label>
         <div class="col-md-6">
-            <select name="school" class="form-control">
+            <select name="school_id" class="form-control">
+                <option disabled selected>Оберіть школу</option>
                 @foreach($schools as $school)
                     <option value="{{$school->id}}">{{$school->name}}</option>
                 @endforeach
