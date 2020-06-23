7ec0a519212cbc34824a631680a54b9aba77e546
AlexPoddubny <usatij.alex@gmail.com>
2020-06-23 17:28:11 +0300

    Users seeding

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,34 +1,35 @@
 <?php
-
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
-
-use App\User;
-use Faker\Generator as Faker;
+    
+    /** @var \Illuminate\Database\Eloquent\Factory $factory */
+    
+    use App\User;
+    use Faker\Generator as Faker;
     use Illuminate\Support\Arr;
+    use Illuminate\Support\Facades\Hash;
     use Illuminate\Support\Str;
-
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
-
-$factory->define(User::class, function (Faker $faker) {
-    $sex = Arr::random(['male', 'female']);
-    return [
-        'firstname' => $faker->firstName($sex),
-        'middlename' => $faker->firstNameMale,
-        'lastname' => $faker->lastName,
-        'phone' => $faker->tollFreePhoneNumber,
-        'email' => $faker->unique()->safeEmail,
-        'sex' => $sex,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
-    ];
-});
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Model Factories
+    |--------------------------------------------------------------------------
+    |
+    | This directory should contain each of the model factory definitions for
+    | your application. Factories provide a convenient way to generate new
+    | model instances for testing / seeding your application's database.
+    |
+    */
+    
+    $factory->define(User::class, function (Faker $faker) {
+        $sex = Arr::random(['male', 'female']);
+        return [
+            'firstname' => $faker->firstName($sex),
+            'middlename' => $faker->firstNameMale,
+            'lastname' => $faker->lastName($sex),
+            'phone' => $faker->phoneNumber,
+            'email' => $faker->unique()->safeEmail,
+            'sex' => $sex,
+            'email_verified_at' => now(),
+            'password' => Hash::make('123'), // password
+            'remember_token' => Str::random(10),
+        ];
+    });
